6b4f90508edf081a5ab9190ebde687e56b6b853c
Dane Ding <dane.ding@bureauveritas.com>
2026-01-13 14:40:24 +0800

    improve remote entry loader

--- src/utils.ts
+++ src/utils.ts
@@ -17,33 +17,26 @@ export const getRemoteDtsTypesUrls: GetRemoteDtsTypesUrls = (remoteDtsTypeUrls =
 }
 
 const getRemoteEntryLoader: GetRemoteEntryLoader = ({ name, publicPath = '/', origin = '' }) => {
-  return `promise new Promise(resolve => {
-            const now = new Date().getTime();
-            const _origin = !!"${origin}" ? "${origin}" : window.location.origin;
-            const remoteUrl = _origin + '${publicPath}remoteEntry.js?t=' + now;
-            const remoteScriptId = "mf-remotes-${name}";
-            const script = document.createElement('script');
-            script.setAttribute("id", remoteScriptId);
-            script.src = remoteUrl;
-            script.onload = () => {
-              resolve({
-                get: (...request) => {
-                  return window.${name}.get(...request)
-                },
-                init: (...arg) => {
-                  try {
-                    return window.${name}.init(...arg);
-                  } catch (e) {
-                    console.log('remote container already initialized');
-                  }
-                }
-              });
-            };
-            script.onerror = () => {
-              console.log('remote container ${name} load failed');
-            };
-            document.head.appendChild(script);
-          })`;
+  const code = 
+`promise new Promise((resolve, reject) => {
+  if (typeof window.${name} !== "undefined")
+    return resolve(window.${name});
+  const _origin = !!"${origin}" ? "${origin}" : window.location.origin;
+  const now = new Date().getTime();
+  __webpack_require__.l(_origin + "${publicPath}remoteEntry.js?t=" + now, (event) => {
+      if (typeof window.${name} !== "undefined")
+          return resolve(window.${name});
+      var errorType = event && (event.type === 'load' ? 'missing' : event.type);
+      var realSrc = event && event.target && event.target.src;
+      var __webpack_error__ = new Error();
+      __webpack_error__.message = 'Loading script failed.(' + errorType + ': ' + realSrc + ')';
+      __webpack_error__.name = 'ScriptExternalLoadError';
+      __webpack_error__.type = errorType;
+      __webpack_error__.request = realSrc;
+      reject(__webpack_error__);
+  }, "${name}");
+})`
+  return code;
 }
 
 export const getRemoteEntrys: GetRemoteEntrys = (remoteEntryConfs = []) => {
